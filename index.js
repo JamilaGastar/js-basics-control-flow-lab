137e536efd60481035dd4c068e7dab0a054400f5
jamilagastar <jamilagastar@gmail.com>
2022-04-15 14:58:18 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideValue){
+  if (rideValue <= 400) {
+ return 'This one is on me!';
+  } else if (rideValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideValue >= 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(2500));
+
+let city = 'NYC';
+city;
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity('Pittsburgh'));
+
+let tipDescription = "generous";
+
+function switchOnCharmFromTip(tipDescription){
+  switch (tipDescription) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default: 
+      return "Bye."
+  }
 }
+
+console.log(switchOnCharmFromTip("generous"));
